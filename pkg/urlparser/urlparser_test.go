2725c10253ea75af777d357d59759ad657b2ea7c
ayushkr12 <ayushkr91997@gmail.com>
2025-04-17 22:04:21 +0530

    create urlparser package

--- pkg/urlparser/urlparser_test.go
+++ pkg/urlparser/urlparser_test.go
@@ -2,30 +2,9 @@ package urlparser
 
 import (
 	"testing"
-
-	"github.com/ayushkr12/sfz/internal/pkg/ffwrapper"
 )
 
 var rawURLS = []string{"https://example.com/path/to/resource", "https://test.io/another/path"}
-
-func TestRunFUFF(t *testing.T) {
-	fuzzableUrls := []string{"https://example.com/FUZZ", "https://test.io/FUZZ"}
-	finalJSONOutputFilePath := "output.json"
-	ffufResultsOutputFolder := "."
-	wordlistPath := "-"
-	headers := ""
-	disableAutomaticCalibration := false
-	disableColorizeOutput := false
-	silent := false
-	additionalFFUFArgs := []string{"-mc", "200"}
-	debugLog := false
-
-	w := ffwrapper.NewFFUFWrapper(
-		fuzzableUrls, finalJSONOutputFilePath, ffufResultsOutputFolder, wordlistPath, headers, disableAutomaticCalibration, disableColorizeOutput, silent, additionalFFUFArgs, debugLog,
-	)
-
-	w.LaunchCMDs()
-}
 
 func TestGenerateFuzzableURLs(t *testing.T) {
 	fuzzIdentifier := "FUZZ"
